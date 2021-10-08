bf8ce651e8d2c89da924e22fd0627f2961840ed8
Waldo <waldofelix1@gmail.com>
2021-10-08 07:21:04 +0700

    logger json

--- app/middleware/log.go
+++ app/middleware/log.go
@@ -7,7 +7,9 @@ import (
 
 func LogMiddlewareInit(e *echo.Echo) {
 	logger := middleware.LoggerConfig{
-		Format: `[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}` + "\n",
+		Format: `{"time":"${time_rfc3339_nano}","remote_ip":"${remote_ip}",` +
+    			`"method":"${method}","host":"${host}","uri":"${uri}",` +
+    			`"status":${status},"error":"${error}","latency":${latency_human}}` + "\n",
 	}
 
 	e.Use(middleware.LoggerWithConfig(logger))
